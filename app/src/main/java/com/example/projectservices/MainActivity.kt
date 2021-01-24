02231450e1102ef42040325b394627cd20df403c
Admin <makansari@gmail.com>
2021-01-24 16:54:50 +0530

    Alert Dialog

--- app/src/main/java/com/example/projectservices/MainActivity.kt
+++ app/src/main/java/com/example/projectservices/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.projectservices
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +21,27 @@ class MainActivity : AppCompatActivity() {
         buttonStop.setOnClickListener {
             var myIntent = Intent(this,MyService::class.java)
             stopService(myIntent)
+        }
+
+        buttonAlert.setOnClickListener {
+
+            var myBuilder = AlertDialog.Builder(this)
+            myBuilder.setTitle("ALERT !!!")
+            myBuilder.setMessage("Do you want to continue with Actvity ? ")
+
+          myBuilder.setPositiveButton("OK",DialogInterface.OnClickListener { dialog, which ->
+
+            Toast.makeText(this, "COOL", Toast.LENGTH_SHORT).show()
+          })
+
+            myBuilder.setNegativeButton("EXIT", DialogInterface.OnClickListener { dialog, which ->
+
+                    finish()  // will finish the activity
+            })
+
+            var myalertDialog = myBuilder.create()
+            myalertDialog.show()
+
         }
     }
 }
--- app/src/main/java/com/example/projectservices/SplashActivity.kt
+++ app/src/main/java/com/example/projectservices/SplashActivity.kt
@@ -4,21 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.Gravity
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_splash.*
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         var handler = Handler()
-
-        var th = Thread(){
+       /* var th = Thread(){
             kotlin.run {
                 for(i in 0..5){
-                    /*handler.post {
+                    *//*handler.post {
                         textViewSplash.setText( "" + i)
                         //test
-                    }*/
+                    }*//*
 
                         runOnUiThread {
                             textViewSplash.setText( "" + i)
@@ -32,6 +36,31 @@ class SplashActivity : AppCompatActivity() {
 
 
         }
-        th.start()
+        th.start()*/
+
+            var arr = arrayOf("SELECT ONE", "test1","test2","test3")
+
+        var myadappter = ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,arr)
+
+
+
+
+        myadappter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+       spinnersplash.adapter = myadappter
+
+        spinnersplash.onItemSelectedListener = this
+
+
+
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+        var data = parent?.getItemAtPosition(position).toString()
+        textViewSplash.setText(data)
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 }
