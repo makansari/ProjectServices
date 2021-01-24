4ad9618ee9602889928c4f0fc05526bcc99ed494
Admin <makansari@gmail.com>
2021-01-24 13:03:38 +0530

    Serivces properties

--- app/src/main/java/com/example/projectservices/MyService.kt
+++ app/src/main/java/com/example/projectservices/MyService.kt
@@ -2,11 +2,13 @@ package com.example.projectservices
 
 import android.app.Service
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.IBinder
 import android.widget.Toast
 
 class MyService : Service() {
 
+   lateinit var mp : MediaPlayer
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
     }
@@ -14,10 +16,12 @@ class MyService : Service() {
     override fun onCreate() {
         super.onCreate()
         Toast.makeText(this, "Service Created", Toast.LENGTH_SHORT).show()
+        mp = MediaPlayer.create(this, R.raw.mymusic)
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Toast.makeText(this, "Service Started", Toast.LENGTH_SHORT).show()
+        mp.start()
 
         return super.onStartCommand(intent, flags, startId)
     }
@@ -25,6 +29,6 @@ class MyService : Service() {
     override fun onDestroy() {
         super.onDestroy()
         Toast.makeText(this, "Service Destroyed", Toast.LENGTH_SHORT).show()
-
+        mp.stop()
     }
 }
