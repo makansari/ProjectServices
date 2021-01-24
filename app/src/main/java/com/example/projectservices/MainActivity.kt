924de52a7953910015d4da5b7729823b132ccc5f
Admin <makansari@gmail.com>
2021-01-24 17:59:27 +0530

    Spinner

--- app/src/main/java/com/example/projectservices/MainActivity.kt
+++ app/src/main/java/com/example/projectservices/MainActivity.kt
@@ -4,14 +4,19 @@ import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var countries = arrayOf("Saudi Arabia", "India", "Pakistan", "China", "Afghanisthan")
 
         buttonStart.setOnClickListener {
             var myIntent = Intent(this,MyService::class.java)
@@ -45,5 +50,24 @@ class MainActivity : AppCompatActivity() {
             myalertDialog.show()
 
         }
+
+            var myArrayAdapter = ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,countries)
+
+       myArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinnerCountry.adapter = myArrayAdapter
+
+        spinnerCountry.onItemSelectedListener = this
+
+
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+      var data = parent?.getItemAtPosition(position)
+        Toast.makeText(this,"you selected : $data which is at $position",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
     }
 }
